dade9c500ebd425d2ed7cdfeeab8fcfb8f7df09a
Vostafi69 <thorik.sanya@gmail.com>
2023-05-28 19:04:09 +1000

    Представление для сущности занятого

---
+++ ExchangeProject/Views/EmployerView/EmployerView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExchangeProject.Views.EmployerView
+{
+    public partial class EmployerView : Form
+    {
+        public EmployerView()
+        {
+            InitializeComponent();
+        }
+    }
+}
